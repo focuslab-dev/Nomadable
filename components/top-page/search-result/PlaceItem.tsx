d33437faba2ef582671cd4fd74ee420518c3195c
白水佑哉 <ys0520@gmail.com>
2024-07-08 23:11:29 +0900

    fix review text style on place list

--- components/top-page/search-result/PlaceItem.tsx
+++ components/top-page/search-result/PlaceItem.tsx
@@ -110,13 +110,7 @@ const TopReview: React.FC<{ topReview: ReviewSimpleType }> = ({
   return (
     <ReviewInfo>
       <UserPicture src={topReview.userPicture} />
-      <Comment
-        style={{
-          fontSize: "0.5rem",
-        }}
-      >
-        {topReview.comment}
-      </Comment>
+      <Comment>{topReview.comment}</Comment>
     </ReviewInfo>
   );
 };
@@ -281,5 +275,5 @@ const UserPicture = styled.img`
 const Comment = styled.div`
   white-space: nowrap;
   color: ${cons.FONT_COLOR_SECONDARY};
-  font-size: 0.5rem;
+  font-size: 13px;
 `;
